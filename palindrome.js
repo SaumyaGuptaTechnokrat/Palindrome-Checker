5fd2538fefb2804b375893d9135204b02ca23206
SaumyaGuptaTechnokrat <65768914+SaumyaGuptaTechnokrat@users.noreply.github.com>
2023-01-18 02:30:16 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,19 @@
 function palindrome(){
-    
-}
+            var data = document.getElementById('pal').value;
+            var rev='';
+            for(var i=0;i<data.length;i++){
+                rev = data.charAt(i)+rev; 
+            } 
+            if(rev==data){
+                    document.getElementById('head1').innerHTML='IT IS A PALINDROME';
+            }
+            else{
+                document.getElementById('head1').innerHTML='IT IS NOT A PALINDROME';
+            }    
+        }
+            var blink = document.getElementById('blink');
+  
+        setInterval(function () {
+            blink.style.opacity = 
+            (blink.style.opacity == 0 ? 1 : 0);
+        }, 500);
